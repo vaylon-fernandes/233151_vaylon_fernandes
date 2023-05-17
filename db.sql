2a33caa800d5e3b18fb72d0977bc5f127bcfc141
dac <dac@dypm.org>
2023-05-17 15:42:29 +0530

    Add new files

--- db.sql
+++ db.sql
@@ -19,5 +19,12 @@ DeptNo int, DeptName varchar(20)
 );
 
 create table Roles(
-RoleTypes varchar(20);
+RoleTypes varchar(20)
 );
+
+create table Manager(
+Mgrno int, Name varchar(20), Email varchar(20), ManagerRole varchar(20), ProjectId int, 
+constraint proj foreign key(ProjectId) references Project(ProjectId)
+);
+
+create table project(ProjectID int, Projectname varchar(20));
